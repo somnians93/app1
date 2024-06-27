80307c4f3f41032c4c2f83a56edb968df31ec7fe
somnians93 <156350809+somnians93@users.noreply.github.com>
2024-06-27 19:27:57 +0300

    Registered simple tag for categories

--- app/settings.py
+++ app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,6 +132,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
     ]
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 INTERNAL_IPS = [
--- main/views.py
+++ main/views.py
@@ -6,12 +6,11 @@ from goods.models import Categories
 
 def index(request):
     
-    categories = Categories.objects.all()
-
+    
     context = {
         'title': 'Home - Главная',
-        'content': "Магазин мебели HOME",
-        'categories': categories
+        'content': "Магазин мебели Мебель HOME",
+        
     }
 
     return render(request, 'main/index.html', context)
