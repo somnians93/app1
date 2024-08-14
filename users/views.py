769c612202dee155533337cfbfec0485a9e3aa3c
somnians93 <156350809+somnians93@users.noreply.github.com>
2024-08-14 20:37:16 +0300

    login required decorator

--- users/views.py
+++ users/views.py
@@ -17,10 +17,13 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
+
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
+
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
-
     context = {
         'title': 'Home - Авторизация',
         'form': form
